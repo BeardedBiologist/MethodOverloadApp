4ad74cb898546f4a9b940b88b310593a4f868224
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 10:45:29 +0200

    init commit. project setup.

--- MethodOverload/Program.cs
+++ MethodOverload/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+
+}
